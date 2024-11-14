88c482b3f7ff1b30c3510673b3b0b2ed9896de7c
Sergio Peixoto <sergiopeixoto3@gmail.com>
2024-11-14 23:08:42 +0000

    made base url in axios config configurable

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: import.meta.env.VITE_APP_API_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',

1bfd0520d2a9973b9fd099d8817e24e341b2e42c
TiagoAzevedo03 <tiagoazevedo03@gmail.com>
2024-11-22 17:11:47 +0000

    playlist model created

---
+++ backend/src/models/playlist.ts
@@ -0,0 +1,45 @@
+import { users } from '@/models/user';
+import { musics } from '@/models/music';
+import { relations } from 'drizzle-orm';
+import {
+  boolean,
+  integer,
+  pgTable,
+  varchar,
+} from 'drizzle-orm/pg-core';
+
+export const playlists = pgTable('playlist', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar().notNull(),
+  public: boolean().default(false),
+  userId: integer()
+    .notNull()
+    .references(() => users.id, { onDelete: 'cascade' }),
+});
+
+export const musicsPlaylists = pgTable('music_playlist', {
+    musicId: integer()
+        .notNull()
+        .references(() => musics.id, { onDelete: 'cascade' }),
+    playlistId: integer()
+        .notNull()
+        .references(() => playlists.id, { onDelete: 'cascade' }),
+});
+
+export const playlistRelations = relations(playlists, ({ one }) => ({
+  user: one(users, {
+    fields: [playlists.userId],
+    references: [users.id],
+  }),
+}));
+
+export const musicPlaylistRelations = relations(musicsPlaylists, ({ one }) => ({
+  music: one(musics, {
+    fields: [musicsPlaylists.musicId],
+    references: [musics.id],
+  }),
+  playlist: one(playlists, {
+    fields: [musicsPlaylists.playlistId],
+    references: [playlists.id],
+  }),
+}));

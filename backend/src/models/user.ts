6929816c865aaf6fc1d91bc2b975c3eaae2d4db8
Fracci <fracci@HP-Fracci>
2024-12-03 13:06:28 +0000

    fixed merge conflicts

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { musics } from '@/db/schema';
 import { relations } from 'drizzle-orm';
 import {
@@ -83,3 +84,90 @@ export const calculateCompletionPercentage = (
 
   return Math.round(percentage);
 };
+=======
+import { musics } from '@/db/schema';
+import { relations } from 'drizzle-orm';
+import {
+  date,
+  integer,
+  json,
+  pgTable,
+  serial,
+  varchar,
+} from 'drizzle-orm/pg-core';
+
+export const users = pgTable('user', {
+  id: serial().primaryKey(),
+  email: varchar().notNull(),
+  password: varchar().notNull(),
+  image: varchar().notNull().default('/uploads/image/profile.jpg'),
+  gender: varchar().default(''),
+  username: varchar().default(''),
+  phone: varchar().default(''),
+  country: varchar().default(''),
+  state: varchar().default(''),
+  date_of_birth: date().defaultNow(),
+  city: varchar().default(''),
+  street: varchar().default(''),
+  completionPercentage: integer().default(0),
+  checklist: json().default({
+    setupAccount: false,
+    personalInformation: false,
+    uploadPhoto: false,
+    contactInformation: false,
+    workInformation: false,
+  }),
+});
+
+export const userRelations = relations(users, ({ many }) => ({
+  musics: many(musics),
+}));
+
+export const updateChecklist = (user: typeof users.$inferSelect): void => {
+  user.checklist = {
+    setupAccount: !!user.email && !!user.password,
+    personalInformation:
+      !!user.username &&
+      !!user.phone &&
+      !!user.country &&
+      !!user.state &&
+      !!user.city &&
+      !!user.street,
+    uploadPhoto: !!user.image,
+    contactInformation:
+      !!user.phone && !!user.country && !!user.state && !!user.city,
+    workInformation: !!user.username,
+  };
+};
+
+export const calculateCompletionPercentage = (
+  user: typeof users.$inferSelect
+): number => {
+  const fields = [
+    user.email,
+    user.password,
+    user.username,
+    user.image,
+    user.phone,
+    user.country,
+    user.state,
+    user.city,
+    user.street,
+    user.date_of_birth,
+    user.image,
+  ];
+
+  let fieldsFilled = 0;
+
+  for (const field of fields) {
+    if (field) {
+      fieldsFilled++;
+    }
+  }
+
+  const totalFields = fields.length;
+  const percentage = (fieldsFilled / totalFields) * 100;
+
+  return Math.round(percentage);
+};
+>>>>>>> b2feb1c (fixed merge conflicts)

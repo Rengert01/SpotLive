362e09ea2ffeefea74d891ddf63eaf730406752d
TheOnis <onisamuelmichael@gmail.com>
2024-12-03 14:55:43 +0000

    +++ merge conflicts resolved

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -3,5 +3,9 @@ export { musics, musicRelations } from '@/models/music';
 export { sessions, sessionRelations } from '@/models/session';
 export { followers, followerRelations } from '@/models/followers';
 export { notifications, notificationRelations } from '@/models/notifications';
-export { albums, albumRelations, musicToAlbums, musicToAlbumsRelations } from '@/models/albums';
-
+export {
+  albums,
+  albumRelations,
+  musicToAlbums,
+  musicToAlbumsRelations,
+} from '@/models/albums';

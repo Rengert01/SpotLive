7d07685838d75d9f1c8d67be9d793374d666fce2
Sergio Peixoto <sergiopeixoto3@gmail.com>
2024-12-03 13:16:11 +0000

    Revert "fixed merge conflicts"
    This reverts commit b2feb1c1111961f532a069a82b833deb1be4d899.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express, { Express } from 'express';
 import env from '../env';
 import cookieParser from 'cookie-parser';
@@ -48,54 +47,3 @@ app.use('/api/album', albumRouter);
 app.listen(port, async () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-=======
-import express, { Express } from 'express';
-import env from '../env';
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-
-import cors from 'cors';
-import corsOptions from '@/config/cors';
-import authRoutes from '@/routes/auth';
-import isAuthenticated from '@/middleware/auth';
-import profileRouter from '@/routes/profile';
-import musicRoutes from '@/routes/music';
-import albumRouter from '@/routes/album';
-
-import FileStore from 'session-file-store';
-const fileStoreOptions = {};
-
-const app: Express = express();
-const port = env.BACKEND_API_PORT ?? 3001;
-
-app.use(cors(corsOptions));
-app.options('*', cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.json());
-
-app.use(
-  session({
-    secret: env.SESSION_SECRET,
-    resave: true,
-    saveUninitialized: true,
-    store: new (FileStore(session))(fileStoreOptions),
-  })
-);
-
-app.use('/api/auth', authRoutes);
-
-// Static images folder
-app.use('/uploads/image', express.static('src/uploads/image'));
-
-// Everything below this line will require authentication
-app.use(isAuthenticated);
-
-app.use('/api/auth', profileRouter);
-app.use('/api/music', musicRoutes);
-app.use('/api/album', albumRouter);
-
-app.listen(port, async () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
->>>>>>> b2feb1c (fixed merge conflicts)

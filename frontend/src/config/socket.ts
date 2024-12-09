942fc1599b933b63d2b4b4163ad7e1ae3311886a
Sergio Peixoto <sergiopeixoto3@gmail.com>
2024-12-09 17:51:28 +0000

    removed unused dependencies

--- frontend/src/config/socket.ts
+++
@@ -1,8 +0,0 @@
-import { io } from 'socket.io-client';
-
-const socket = io(import.meta.env.VITE_APP_API_URL + '/api/livestream', {
-  transports: ['websocket'],
-  withCredentials: true,
-});
-
-export default socket;
